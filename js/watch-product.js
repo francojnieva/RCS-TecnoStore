edde0bea1ec7d0da9a5c402390f2cd15c218bb11
francojnieva <franco97nieva@hotmail.com>
2023-04-18 20:01:49 -0300

    feat::memo: rendered watch products

---
+++ js/watch-product.js
@@ -0,0 +1,51 @@
+
+const containerWatch = document.getElementById("watch-container")
+
+const productWatch = [
+    {
+        marca: "Xiaomi",
+        title: "AMAZFIT GTS 4 MINI",
+        price: 60.300,
+        id: "CODW01",
+        image: "./img/xiaomi-watch1.png"
+    },
+    {
+        marca: "Xiaomi",
+        title: "WATCH S1 ACTIVE GL",
+        price: 59.900,
+        id: "CODW02",
+        image: "./img/xiaomi-watch2.png"
+    },
+    {
+        marca: "Xiaomi",
+        title: "KIESLECT L11 PRO",
+        price: 26.500,
+        id: "CODW03",
+        image: "./img/xiaomi-watch3.png"
+    }
+]
+
+const renderWatch = (product) => {
+    // containerMobile.innerHTML = ""
+    product.forEach(product => {
+        const elementProduct = document.createElement("div"); // crea un nodo con un elemento HTML de tipo div
+
+        elementProduct.classList.add("product") // agrega una clase llamada "product" al div 
+
+        elementProduct.innerHTML = `
+                <div class="card card-mobile">
+                    <img src=${product.image} class="card-img-top" alt="Celular de la marca Samsung">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.marca}</h5>
+                        <p class="card-text">${product.title}</p>
+                        <p class="price price-mobile">$ ${product.price}</p>
+                        <p class="keys">${product.id}</p>
+                        <a href="#" class="btn btn-primary">Ver producto</a>
+                    </div>
+                </div>
+        `
+        containerWatch.appendChild(elementProduct)
+    });
+}
+
+renderWatch(productWatch)
