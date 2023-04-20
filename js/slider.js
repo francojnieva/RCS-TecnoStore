f5c98852e068bf09c8736e2844d0a7bd73346c35
Belén Gómez <gomezbelen86@gmail.com>
2023-04-20 20:37:03 -0300

    feat::memo: add render slider

---
+++ js/slider.js
@@ -0,0 +1,40 @@
+const sliderConteiner= document.getElementById("carouselExampleAutoplaying")
+
+const imagenSlider= [{
+    images1: "./img/slider1.jpg",
+    images2: "./img/slider2.jpg",
+    images3: "./img/slider1-mobile.jpg",
+    images4: "./img/slider2-mobile.jpg"
+
+}
+
+]
+
+
+const renderSlider= (images)=> {
+    images.forEach(images => {
+        const elementSlider= document.createElement("div")
+        elementSlider.innerHTML=`<div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="w-100 slider1-desktop" src=${images.images1} alt="...">
+          <img class="w-100 slider1-mobile" src=${images.imagen3} alt="Banner de televisión">
+        </div>
+        <div class="carousel-item">
+          <img class="w-100 slider2-desktop" src=${images.images2} alt="...">
+          <img class="w-100 slider2-mobile" src=${images.images4} alt="Banner de relojes">
+        </div> 
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button> ` 
+
+
+      sliderConteiner.appendChild(elementSlider)
+    });
+}
+
+renderSlider(imagenSlider)
