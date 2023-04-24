4f43424752f92606b78ca401d3a201cc28d2c323
Santiago Gonzalez <sxntiago01@gmail.com>
2023-04-24 18:42:22 -0300

    feat::memo:add form create user account and details of product cards

--- js/footer.js
+++ js/footer.js
@@ -6,18 +6,24 @@ const itemFooter = [
         name: "Franco Nieva",
         profileLinkedin: "https://www.linkedin.com/in/francojnieva",
         profileGithub: "https://github.com/francojnieva",
+        imagelinkedin: "../img/linkedin_logo.svg",
+        imagegithub: "../img/github_logo.svg",
     },
 
     {
         name: "Belén Gómez",
         profileLinkedin: "https://www.linkedin.com/in/beléngómez",
         profileGithub: "https://github.com/belugomez",
+        imagelinkedin: "../img/linkedin_logo.svg",
+        imagegithub: "../img/github_logo.svg",
     },
 
     {
         name: "Santiago González",
         profileLinkedin: "https://www.linkedin.com/in/santiago-gonzalez-95bb14243",
         profileGithub: "https://github.com/Sxntiago01",
+        imagelinkedin: "../img/linkedin_logo.svg",
+        imagegithub: "../img/github_logo.svg",
     },
  
 ]
@@ -30,10 +36,10 @@ const renderFooter = (item) => {
 
                 <p class= "members text-center mt-2">${item.name}</p>
                 <a class="text-decoration-none" href="${item.profileLinkedin}" target="_blank">
-                 <img class="linkedin" src="../img/linkedin_logo.svg" alt="Linkedin" >
+                 <img class="linkedin" src="${item.imagelinkedin}" alt="Linkedin" >
                 </a>
                 <a class="text-decoration-none" href="${item.profileGithub}" target="_blank">
-                <img class="github" src="../img/github_logo.svg" alt="Github">
+                <img class="github" src="${item.imagegithub}" alt="Github">
                 </a>
         `
         containerFooter.appendChild(elementitem)
